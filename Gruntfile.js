c33e7775aa96cbbb39802040533f50e88d9555db
microdog <dlangu0393@gmail.com>
2016-02-02 15:58:37 +0800

    Add script to crawl data from Baidu Open CDN Service(http://cdn.code.baidu.com/).

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,23 @@
 'use strict';
 
 module.exports = function (grunt) {
+  grunt.initConfig({
+    execute: {
+      update: {
+        src: ['update.js']
+      }
+    }
+  });
+
   grunt.loadNpmTasks('grunt-release');
+  grunt.loadNpmTasks('grunt-execute');
+
+  grunt.registerTask('update', [
+    'execute:update'
+  ]);
+
+  grunt.registerTask('release', [
+    'execute:update',
+    'release'
+  ]);
 };
